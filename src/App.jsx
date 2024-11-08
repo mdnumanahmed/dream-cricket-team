ee02381ba898c560908fa5b94c0a2b4fa025722f
mdnumanahmed <mnumansyl@gmail.com>
2024-11-08 19:20:12 +0600

    Newsletter section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Banner from "./components/Banner/Banner";
 import Header from "./components/Header/Header";
+import NewsLetter from "./components/NewsLetter/NewsLetter";
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
+
+      <NewsLetter />
     </>
   );
 }
---
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,26 @@
+const NewsLetter = () => {
+  return (
+    <div className="container mx-auto bg-[url('../../assets/bg-shadow.png')] bg-no-repeat bg-cover bg-center px-60 py-24 rounded-3xl ring-2 ring-white ring-offset-[24px] ring-offset-white/15">
+      <div className="space-y-4 max-w-4xl text-center">
+        <h3 className="text-4xl font-bold text-[#131313]">
+          Subscribe to our Newsletter
+        </h3>
+        <p className="text-xl font-medium text-[#131313B3]">
+          Get the latest updates and news right in your inbox!
+        </p>
+        <div className="pt-2">
+          <input
+            type="text"
+            placeholder="Enter your email"
+            className="input input-bordered w-full h-14 max-w-xs mr-4"
+          />
+          <button className="text-base font-bold text-[#131313] px-7 py-4 rounded-xl bg-gradient-to-r from-purple-400 hover:from-purple-600 to-orange-400 hover:to-orange-600 hover:text-white">
+            Subscribe
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsLetter;
